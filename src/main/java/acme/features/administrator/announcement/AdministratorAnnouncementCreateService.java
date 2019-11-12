709352867d2830842ff5c6f60931f62a471a8a6f
Adrian <Adrian@10.100.242.14>
2019-11-12 12:54:50 +0100

    Task001 Announcement finalizada

---
+++ src/main/java/acme/features/administrator/announcement/AdministratorAnnouncementCreateService.java
@@ -0,0 +1,83 @@
+
+package acme.features.administrator.announcement;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.announcement.Announcement;
+import acme.framework.components.Errors;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.entities.Administrator;
+import acme.framework.services.AbstractCreateService;
+
+@Service
+public class AdministratorAnnouncementCreateService implements AbstractCreateService<Administrator, Announcement> {
+
+	//Internal State -----------------------------
+	@Autowired
+	AdministratorAnnouncementRepository repository;
+
+
+	@Override
+	public boolean authorise(final Request<Announcement> request) {
+		assert request != null;
+		return true;
+	}
+
+	@Override
+	public void bind(final Request<Announcement> request, final Announcement entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+		request.bind(entity, errors, "moment");
+
+	}
+
+	@Override
+	public void unbind(final Request<Announcement> request, final Announcement entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "moment", "moreInfo", "title", "text");
+
+	}
+
+	@Override
+	public Announcement instantiate(final Request<Announcement> request) {
+		Announcement result = new Announcement();
+		Date moment;
+
+		moment = new Date(System.currentTimeMillis() - 1);
+		result.setMoment(moment);
+		result.setMoreInfo("http://acme.com");
+		result.setTitle("Titulo 1");
+		result.setText("Texto de ejemplo");
+		return result;
+	}
+
+	@Override
+	public void validate(final Request<Announcement> request, final Announcement entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+	}
+
+	@Override
+	public void create(final Request<Announcement> request, final Announcement entity) {
+		assert request != null;
+		assert entity != null;
+		Date moment;
+
+		moment = new Date(System.currentTimeMillis() - 1);
+		entity.setMoment(moment);
+		this.repository.save(entity);
+
+	}
+
+}
